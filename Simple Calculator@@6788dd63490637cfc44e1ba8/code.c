093392340a3afc379ba84787d3809e9e06a3e48f
anshikas09 <anshikasam1810@gmail.com>
2025-01-25 23:45:06 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,10 +4,10 @@ int main() {
     char so;
     scanf("%d %d %c",&n1,&n2,&so);
     switch(so){
-        case '+': printf("%d\n",n1+n2); break;
-        case '-': if(n1>n2){printf("%d\n",n1-n2);} else{printf("\n");} break;
-        case '*': printf("%d\n",n1*n2); break;
-        case '/': if(n2!=0){printf("%d\n",n1/n2);} else{printf("\n");}  break;
+        case '+': printf("%d",n1+n2); break;
+        case '-': printf("%d",n1-n2); break;
+        case '*': printf("%d",n1*n2); break;
+        case '/': if(n2!=0){printf("%d",n1/n2);} else{printf("\n");}  break;
         default: printf("\n");
     }
     return 0;
