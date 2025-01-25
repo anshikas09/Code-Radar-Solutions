e274b6e00020acc0589ab6fe68f38fc9f1c302ce
anshikas09 <anshikasam1810@gmail.com>
2025-01-25 23:37:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %d %c",&n1,&n2,&so);
     switch(so){
         case '+':printf("%d",n1+n2); break;
-        case '-':printf("%d",n1-n2); break;
+        case '-':if(n1>n2){printf("%d",n1-n2);}else{printf("\n");} break;
         case '*':printf("%d",n1*n2); break;
         case '/':if(n2!=0){printf("%d",n1/n2);} else{printf("\n");} break;
         default: printf("\n");
