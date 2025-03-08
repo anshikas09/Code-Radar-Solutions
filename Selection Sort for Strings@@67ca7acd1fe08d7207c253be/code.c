a8ed5941d3f02dc2cb5c4945514869c3a7cbc461
Anshika <anshikasam1810@gmail.com>
2025-03-09 01:16:15 +0530

    Solution to Selection Sort for Strings

--- Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
+++ Selection Sort for Strings@@67ca7acd1fe08d7207c253be/code.c
@@ -3,7 +3,7 @@ void selectionSort(char* arr[],int n){
     for(int i=0;i<n-1;i++){
         minIndex=i;
         for(int j=i+1;j<n;j++){
-            if(strcmp(arr[j],arr[,minIndex])<0){minIndex=j;}
+            if(strcmp(arr[j],arr[minIndex])<0){minIndex=j;}
         }
         if(minIndex!=i){
             temp=arr[i];
