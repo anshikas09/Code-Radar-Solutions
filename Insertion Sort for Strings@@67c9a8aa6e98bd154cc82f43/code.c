22c0e75f157f56e2dbfa0ed3daaf0aa170af179a
Anshika <anshikasam1810@gmail.com>
2025-03-20 16:46:37 +0530

    Solution to Insertion Sort for Strings

---
+++ Insertion Sort for Strings@@67c9a8aa6e98bd154cc82f43/code.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+void insertionSort(char arr[][100],int n){
+    int j;
+    for(int i=1;i<n;i++){
+        char key=arr[i];
+        j=i-1;
+        while(j>=0 && strcmp(arr[j],key)>0){
+            arr[j+1]=arr[j];
+            j--;
+        }
+        arr[j+1]=key;
+    }
+}
+void printArray(char arr[][100],int n){
+    for(int i=0;i<n;i++){
+        printf("%s ",arr[i]);
+    }printf("\n");
+}
