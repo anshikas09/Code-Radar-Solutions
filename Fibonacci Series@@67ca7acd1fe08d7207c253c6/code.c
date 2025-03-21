5097388fe8761d19229ee5cf67d8baee0c3e7e2a
Anshika <anshikasam1810@gmail.com>
2025-03-21 11:53:38 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 // Function to print Fibonacci series up to n
 void fibonacciSeries(int n) {
     int a = 0, b = 1;
 
     // Print Fibonacci numbers up to n
-    while (a < n) {
-        printf("%d ", a);
-        // Generate the next Fibonacci number
+    printf("%d ", a);  // Always print the first Fibonacci number (0)
+    while (b <= n) {
+        printf("%d ", b);  // Print the current Fibonacci number
         int next = a + b;
         a = b;
         b = next;
