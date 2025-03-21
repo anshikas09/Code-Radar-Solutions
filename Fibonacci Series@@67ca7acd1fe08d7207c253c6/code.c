e52a24dd4a961e0274f9b2d7aa3987f64a23257e
Anshika <anshikasam1810@gmail.com>
2025-03-21 11:51:39 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+// Function to print Fibonacci series up to n
+void fibonacci(int n) {
+    int a = 0, b = 1;
+
+    // Print Fibonacci numbers up to n
+    while (a <= n) {
+        printf("%d ", a);
+        // Generate the next Fibonacci number
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    printf("\n");
+}
