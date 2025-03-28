802e289be4f7423c0be1b268e8a040b1cabecb68
Anshika <anshikasam1810@gmail.com>
2025-03-29 00:39:07 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,34 +1,77 @@
+// #include<stdio.h>
+// void SecondLarge(int arr[],int n){
+//     if(n<=1){
+//         printf("-1");
+//         return;
+//     }
+//     int max1,max2;
+//     if(arr[0]>arr[1]){
+//         max1=arr[0];
+//         max2=arr[1];
+//     }if(arr[0]<arr[1]){
+//         max1=arr[1];
+//         max2=arr[0];
+//     }
+//     for(int i=2;i<n;i++){
+//         if(arr[i]>max1){
+//             max2=max1;
+//             max1=arr[i];
+//         }else if(arr[i]>max2 && arr[i]<max1){
+//             max2=arr[i];
+//         }
+//     }
+//     printf("%d",max2);
+// }
+// int main(){
+//     int n;
+//     scanf("%d ",&n);
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         scanf("%d",&arr[i]);
+//     }
+//     SecondLarge(arr,n);
+//     return 0;
+// }
+
+
 #include<stdio.h>
-void SecondLarge(int arr[],int n){
-    if(n<=1){
+
+void SecondLarge(int arr[], int n) {
+    if (n <= 1) {
         printf("-1");
         return;
     }
-    int max1,max2;
-    if(arr[0]>arr[1]){
-        max1=arr[0];
-        max2=arr[1];
-    }if(arr[0]<arr[1]){
-        max1=arr[1];
-        max2=arr[0];
-    }
-    for(int i=2;i<n;i++){
-        if(arr[i]>max1){
-            max2=max1;
-            max1=arr[i];
-        }else if(arr[i]>max2 && arr[i]<max1){
-            max2=arr[i];
+
+    int max1 = arr[0], max2 = -1; 
+    int allSame = 1; // Flag to check if all elements are the same
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != arr[i-1]) {
+            allSame = 0;
+        }
+        
+        if (arr[i] > max1) {
+            max2 = max1;
+            max1 = arr[i];
+        } else if (arr[i] > max2 && arr[i] < max1) {
+            max2 = arr[i];
         }
     }
-    printf("%d",max2);
-}
-int main(){
-    int n;
-    scanf("%d ",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);
+
+    if (allSame || max2 == -1) {
+        printf("-1");
+    } else {
+        printf("%d", max2);
     }
-    SecondLarge(arr,n);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    SecondLarge(arr, n);
     return 0;
 }
