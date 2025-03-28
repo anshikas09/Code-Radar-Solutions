549aae344967652765ee14597376630dad85d3d6
Anshika <anshikasam1810@gmail.com>
2025-03-29 00:22:38 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,54 +1,20 @@
-// #include<stdio.h>
-// void Countfrequency(int arr[],int n){
-//     int visited[n];
-//     for(int i=0;i<n;i++){
-//         if(visited[i]==1){
-//             continue;
-//         }
-    
-//     int count=1;
-    
-//         for(int j=i+1;j<n;j++){
-//             if(arr[i]==arr[j]){
-//                 count++;
-//                 visited[j]=1;
-//             }
-//         }
-//      printf("%d %d\n",arr[i],count);   
-//     }
-// }
-// int main(){
-//     int n;
-//     scanf("%d",&n);
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         scanf("%d",&arr[i]);
-//     }
-//     int visited[n];
-//     for(int i=0;i<n;i++){
-//         visited[i]=0;
-//     }
-//     Countfrequency(arr,n);
-//     return 0;
-// }
-
-
 #include<stdio.h>
-void Countfre(int arr[],int n){
-    int visited=-1;
-    int j;
+void Countfrequency(int arr[],int n){
+    int visited[n];
     for(int i=0;i<n;i++){
-        int count=1;
+        if(visited[i]==1){
+            continue;
+        }
+    
+    int count=1;
+    
         for(int j=i+1;j<n;j++){
             if(arr[i]==arr[j]){
                 count++;
-                arr[j]=visited;
+                visited[j]=1;
             }
-             if(arr[j]!=visited){
-            arr[i]=count;
         }
-        }
-        printf("%d %d ",arr[i],count);
+     printf("%d %d\n",arr[i],count);   
     }
 }
 int main(){
@@ -62,6 +28,6 @@ int main(){
     for(int i=0;i<n;i++){
         visited[i]=0;
     }
-    Countfre(arr,n);
+    Countfrequency(arr,n);
     return 0;
 }
