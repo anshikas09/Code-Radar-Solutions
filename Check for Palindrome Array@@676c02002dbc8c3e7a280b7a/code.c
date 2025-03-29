396a7caefffc4d0b5a81a60e7e6fa2ff27c756f0
Anshika <anshikasam1810@gmail.com>
2025-03-29 10:41:34 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,21 +1,31 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int isPalindrome(int arr[], int n) {
+    int i, j;
+    for (i = 0, j = n - 1; i < j; i++, j--) {
+        if (arr[i] != arr[j]) {
+            return 0; // Not a palindrome
+        }
+    }
+    return 1; // Palindrome
+}
+
+int main() {
     int n;
-    int i,j;
-    scanf("%d",&n);
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
     int arr[n];
-    for(int i=0;i<n;i++){
-    scanf("%d",&arr[n]);
+    printf("Enter the elements: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    for(i=0,j=n-1;i<j;i++,j--){
-        if(arr[i]!=arr[j]){
-            printf("NO");
-            break;
-        }
-        else{
-            printf("YES");
-        }
+
+    if (isPalindrome(arr, n)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
     }
-    
+
     return 0;
 }
