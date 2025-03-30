d5197aae956798ba171efb79270870f2533d3326
Anshika <anshikasam1810@gmail.com>
2025-03-30 12:49:27 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
-#include <limits.h>
+
 int findFirstRepeating(int arr[], int n) {
-    int freq[100000] = {0}; // Assuming elements are within a fixed range
-    int index[100000];
-    for (int i = 0; i < 100000; i++) {
-        index[i] = -1; // Initialize index to -1
-    }
-    // Traverse the array to track frequencies and first occurrences
     for (int i = 0; i < n; i++) {
-        int num = arr[i];
-        freq[num]++;
-        if (index[num] == -1) {
-            index[num] = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                return arr[i]; // Return the first repeating element
+            }
         }
     }
-    int minIndex = INT_MAX;
-    // Find the first repeating element
-    for (int i = 0; i < n; i++) {
-        if (freq[arr[i]] > 1 && index[arr[i]] < minIndex) {
-            minIndex = index[arr[i]];
-        }
-    }
-    return (minIndex == INT_MAX) ? -1 : arr[minIndex];
+    return -1; // Return -1 if no repeating element is found
 }
+
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
+
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
+
     int result = findFirstRepeating(arr, n);
-    if (result == -1) {
-        printf("-1");
-    } else {
-        printf("%d\n", result);
-    }
+    printf("%d", result);
+
     return 0;
 }
