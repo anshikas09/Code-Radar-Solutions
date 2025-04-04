784b8c0a7c77503da56d8d6e9320964192100b86
Anshika <anshikasam1810@gmail.com>
2025-04-04 16:51:18 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -1,43 +1,48 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
+// Check if string contains only spaces
 int isOnlySpaces(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != ' ') {
-            return 0; // Found non-space character
+            return 0; // Found something other than space
         }
     }
-    return 1; // All spaces
+    return 1; // All characters are spaces
 }
 
+// Check if string contains only 0s and 1s
 int isBinary(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != '0' && str[i] != '1') {
-            return 0; // Not binary
+            return 0; // Found non-binary character
         }
     }
-    return 1; // All 0s and 1s
+    return 1; // All binary
 }
 
 int main() {
     char str[1000];
 
+    printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-    str[strcspn(str, "\n")] = '\0'; // Remove newline
 
+    // Remove trailing newline from fgets
+    str[strcspn(str, "\n")] = '\0';
+
+    // If empty string
     if (strlen(str) == 0) {
-        printf("No\n"); // Empty string is not valid
-    }
-    else if (isOnlySpaces(str)) {
-        printf("Yes\n");
-    }
-    else if (isBinary(str)) {
-        printf("Yes\n");
-    }
-    else {
         printf("No\n");
     }
+    else if (isOnlySpaces(str)) {
+        printf("Yes\n"); // Only spaces
+    }
+    else if (isBinary(str)) {
+        printf("Yes\n"); // Only 0s and 1s
+    }
+    // else {
+    //     printf("No\n"); // Everything else
+    // }
 
     return 0;
 }
