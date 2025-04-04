9c302c3beaeaac8399021ff9792c2b08b1b5b4ad
Anshika <anshikasam1810@gmail.com>
2025-04-04 12:15:52 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-
 // Helper function to reverse characters in a portion of a string
 void reverse(char *str, int start, int end) {
     while (start < end) {
@@ -9,40 +8,28 @@ void reverse(char *str, int start, int end) {
         str[end] = tmp;
         start++;
         end--;
-    }
-}
-
+    }}
 // Function to reverse words in a string
 void reverseWords(char *str) {
     int len = strlen(str);
-
     // Remove trailing newline if exists (from fgets)
     if (len > 0 && str[len - 1] == '\n') {
         str[len - 1] = '\0';
         len--;  // Update length after removing newline
     }
-
     // Step 1: Reverse the entire string
     reverse(str, 0, len - 1);
-
     // Step 2: Reverse each word
     int start = 0;
     for (int i = 0; i <= len; i++) {
         if (str[i] == ' ' || str[i] == '\0') {
             reverse(str, start, i - 1);
             start = i + 1;
-        }
-    }
-}
-
+        }}}
 int main() {
     char str[1000];
-
     fgets(str, sizeof(str), stdin);
-
     reverseWords(str);
-
     printf("%s\n", str);
-
     return 0;
 }
