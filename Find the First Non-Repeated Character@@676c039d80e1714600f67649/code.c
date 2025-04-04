dc50fd9029f2c7a3d909b1b1583e768d15851069
Anshika <anshikasam1810@gmail.com>
2025-04-04 18:53:59 +0530

    Solution to Find the First Non-Repeated Character

--- Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
+++ Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 #include <string.h>
-#define CHAR_RANGE 256  // Total number of possible characters
+
 char firstNonRepeatedChar(const char *str) {
-    int freq[CHAR_RANGE] = {0};
-    int i;
-    // Count frequency of each character
-    for (i = 0; str[i]; i++) {
-        freq[(unsigned char)str[i]]++;
+    int freq[256] = {0};
+
+    // Count frequencies
+    for (int i = 0; str[i] != '\0'; i++) {
+        freq[(int)str[i]]++;
     }
-    // Find the first character with frequency 1
-    for (i = 0; str[i]; i++) {
-        if (freq[(unsigned char)str[i]] == 1) {
+
+    // Find the first non-repeated character
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (freq[(int)str[i]] == 1) {
             return str[i];
-        }}
-    return '\0';  // No non-repeated character found
+        }
+    }
+
+    return '\0'; // No non-repeated character found
 }
+
 int main() {
     char str[200];
     fgets(str, sizeof(str), stdin);
-    // Remove newline character if present
-    size_t len = strlen(str);
-    if (len > 0 && str[len - 1] == '\n')
-        str[len - 1] = '\0';
+
+    // Trim newline character
+    str[strcspn(str, "\n")] = '\0';
+
     char result = firstNonRepeatedChar(str);
+
     if (result != '\0') {
         printf("%c\n", result);
     } else {
         printf("-\n");
     }
+
     return 0;
 }
