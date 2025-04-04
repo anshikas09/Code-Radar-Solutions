b9bcb7b01b59e959a32820a67ecf01789cda3c1f
Anshika <anshikasam1810@gmail.com>
2025-04-04 11:41:48 +0530

    Solution to Find the Length of a String

--- Find the Length of a String@@676c039d80e1714600f67643/code.c
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -1,31 +1,46 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
-    char str[1000];  // Assuming max input length is 1000 characters
-    fgets(str, sizeof(str), stdin);  // Read input including spaces
+    char str[1000];
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  // Remove newline
 
-    // Remove trailing newline character if present
-    str[strcspn(str, "\n")] = '\0';
-
-    int length = 0;
-    int non_space_found = 0;  // Flag to check if there's any non-space character
-
+    // Check if all characters are spaces
+    int all_spaces = 1;
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] != ' ') {
-            non_space_found = 1;  // Found at least one non-space character
+        if (!isspace(str[i])) {
+            all_spaces = 0;
+            break;
         }
     }
 
-    // If input contains only spaces, return 0
-    if (!non_space_found) {
+    if (all_spaces) {
         printf("0\n");
         return 0;
     }
 
-    // If input has words, count all characters including spaces
-    length = strlen(str);
-    printf("%d\n", length);
+    // Count words
+    int word_count = 0;
+    int in_word = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isspace(str[i])) {
+            if (!in_word) {
+                in_word = 1;
+                word_count++;
+            }
+        } else {
+            in_word = 0;
+        }
+    }
+
+    // If no word or only spaces
+    if (word_count == 0) {
+        printf("0\n");
+    } else {
+        printf("%lu\n", strlen(str));
+    }
 
     return 0;
 }
