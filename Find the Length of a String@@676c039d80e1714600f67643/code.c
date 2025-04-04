41de274c87e6ed832043c3c7cb2f762f523eb7de
Anshika <anshikasam1810@gmail.com>
2025-04-04 11:45:53 +0530

    Solution to Find the Length of a String

--- Find the Length of a String@@676c039d80e1714600f67643/code.c
+++ Find the Length of a String@@676c039d80e1714600f67643/code.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-
 int main() {
     char str[1000];  // Buffer to hold input
     fgets(str, sizeof(str), stdin);  // Read input including spaces
-
     // Remove trailing newline if present
     str[strcspn(str, "\n")] = '\0';
-
     // Check if all characters are spaces
     int has_non_space = 0;
     for (int i = 0; str[i] != '\0'; i++) {
         if (!isspace((unsigned char)str[i])) {
             has_non_space = 1;
             break;
-        }
-    }
-
+        }}
     if (!has_non_space) {
         // Input has only spaces
         printf("0\n");
@@ -25,6 +20,5 @@ int main() {
         // Input has at least one non-space character
         printf("%lu\n", strlen(str));
     }
-
     return 0;
 }
