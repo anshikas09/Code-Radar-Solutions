9335d5a1636863e70c4b727266a3b6141d6dfd97
Anshika <anshikasam1810@gmail.com>
2025-04-04 16:49:59 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -1,21 +1,40 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
 int isBinary(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] != '0' && str[i] != '1') {
             return 0; // Not binary
-        }}
+        }
+    }
     return 1; // It's binary
 }
+
+int isOnlySpaces(char *str) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (!isspace(str[i])) {
+            return 0; // Found a non-space character
+        }
+    }
+    return 1; // All characters are spaces
+}
+
 int main() {
     char str[1000];
+
     fgets(str, sizeof(str), stdin);
-    // Remove newline character if present
-    str[strcspn(str, "\n")] = '\0';
-    if (isBinary(str))
-        printf("Yes\n");
-    else
+    str[strcspn(str, "\n")] = '\0'; // Remove newline
+
+    if (isOnlySpaces(str)) {
+        printf("Yes\n"); // String contains only spaces
+    }
+    else if (isBinary(str)) {
+        printf("Yes\n"); // String is binary (only 0 and 1)
+    }
+    else {
         printf("No\n");
+    }
 
     return 0;
 }
