409b82c2394b32fcf6a133a63a461e6a39c22560
Anshika <anshikasam1810@gmail.com>
2025-04-04 12:20:29 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -1,36 +1,43 @@
 #include <stdio.h>
 #include <string.h>
-void reverseWords(char str[]) {
-    int start = 0, end = 0, length = strlen(str);
-    // Reverse the entire string first
-    for (int i = 0; i < length / 2; i++) {
-        char temp = str[i];
-        str[i] = str[length - i - 1];
-        str[length - i - 1] = temp;
-    }
-    // Now reverse each word in the string
-    while (str[end] != '\0') {
-        if (str[end] == ' ' || str[end] == '\0') {
-            int wordEnd = end - 1;
-            while (start < wordEnd) {
-                char temp = str[start];
-                str[start] = str[wordEnd];
-                str[wordEnd] = temp;
-                start++;
-                wordEnd--;
-            }
-            start = end + 1;
-        }
-        end++;
+
+void reverseWord(char* str, int start, int end) {
+    // Reverse characters of a word between start and end index
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+        start++;
+        end--;
     }
 }
+
+void reverseWordsInString(char* str) {
+    int n = strlen(str);
+    int start = 0;
+    
+    // Reverse each word in the string
+    for (int i = 0; i < n; i++) {
+        if (str[i] == ' ' || str[i] == '\0') {
+            reverseWord(str, start, i - 1);  // Reverse the word
+            start = i + 1;  // Update the start of the next word
+        }
+    }
+    // Reverse the last word
+    reverseWord(str, start, n - 1);
+}
+
 int main() {
     char str[100];
-    fgets(str, sizeof(str), stdin);  // Read the string
-    str[strcspn(str, "\n")] = '\0';  // Remove trailing newline if present
-    reverseWords(str);
+    
+    // Input the string
+
+    fgets(str, sizeof(str), stdin);
+    str[strcspn(str, "\n")] = '\0';  // Remove the trailing newline character
+    
+    reverseWordsInString(str);
+    
     printf("%s\n", str);
     
     return 0;
 }
-
